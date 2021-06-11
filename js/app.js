fb0bbe3f59cdd0db2a91e49ec22290929aef7276
navidev0 <juand.ns07@gmail.com>
2021-06-10 21:59:36 -0300

    function setTime

--- js/app.js
+++ js/app.js
@@ -12,8 +12,30 @@ toggle.addEventListener('click', (e) => {
   const html = document.querySelector('html');
   if (html.classList.contains('dark')) {
     html.classList.remove('dark');
-    e.target.innerHTML = 'Dark Mode'
-  }else
+    e.target.innerHTML = 'Dark mode'
+  }else{
     html.classList.add('dark');
-    e.target.innerHTML = 'Ligth Mode'
+    e.target.innerHTML = 'Light mode'
+  }
 });
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursClock = hours >= 13 ? hours % 12 : hours;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM'
+
+  hourVar.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+  minuteVar.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+  secondVar.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+  timeVar.innerHTML = `${hoursClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateVar.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+
